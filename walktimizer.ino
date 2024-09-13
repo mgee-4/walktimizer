29d0c05f0c7c663d7f947cd97f1b24c7e61c0d7c
Marcus Gee <72676962+mgee-4@users.noreply.github.com>
2024-09-12 21:51:26 -0600

    Speed changed to pace, setup for buttons and state machine

--- walktimizer.ino
+++ walktimizer.ino
@@ -3,16 +3,20 @@
 #include <Adafruit_SSD1306.h>
 #include <TinyGPSMinus.h>
 
-#define SCREEN_WIDTH  128 
+#define SCREEN_WIDTH  128
 #define SCREEN_HEIGHT 64
 
 #define OLED_RESET -1
 #define SCREEN_ADDRESS 0x3C // I2C address, either 0x3D or 0x3C
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 TinyGPSMinus gps;
-int leg_length;
-int mass;
-float d_speed = 1.0;
+float legLength = 1;
+float optimalSpeed;
+float optimalPace;
+
+// Define the button pin
+const int upPin = 20;
+const int downPin = 23;
 
 void setup() {
   Serial.begin(9600);
@@ -22,62 +26,119 @@ void setup() {
   Serial1.println("$PMTK220,200*2C");  // Set update rate to 200ms
   delay(1000);
 
-  if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+  // Configure the button pin
+  pinMode(downPin, INPUT_PULLUP);  // Set the button pin as input with internal pull-up resistor
+  pinMode(upPin, INPUT_PULLUP);  // Set the button pin as input with internal pull-up resistor
+
+
+  if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) { // Have a display showing what each field indicates (ex: Optimal Speed, Current Speed, Efficiency)
     Serial.println(F("SSD1306 allocation failed"));
     for (;;);
   }
   display.clearDisplay();
+  updateOptimal();  // Placeholder, need to implement it so that this is called when users adjust leg length manually
   delay(1000);
 }
 
-//Need to set up FSM between user input mode and speed checking/displaying, currently just printing speed/efficiency
+
+// Need to set up FSM between user input mode and speed checking/displaying, currently just printing speed/efficiency
 void loop() {
-  while (Serial1.available()) {
-    char c = Serial1.read();
-    if (gps.encode(c)) {
-      displaySpeed();
+  bool upPressed = digitalRead(upPin) == LOW;
+  bool downPressed = digitalRead(downPin) == LOW;
+
+  if (upPressed) {
+    displayUpButtonPressed();  // Display "UP" message when up button is pressed
+  } else if (downPressed) {
+    displayDownButtonPressed();  // Display "DOWN" message when down button is pressed
+  } else {
+    while (Serial1.available()) {
+      char c = Serial1.read();
+      if (gps.encode(c)) {
+        displayPace();  // Display pace if GPS data is available
+      }
     }
   }
-
 }
 
-// A function that will be used to update user parameters, will change  the ideal speed
-// Need to read up on a good model to use to calculate ideal speed (resonant frequencies)
-// void updateParameters(){
-  
-// }
+
+// Temporary function to test button inputs and debouncing
+void displayDownButtonPressed() {
+  display.clearDisplay();
+  display.setTextSize(2);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0, 0);
+  display.println("DOWN");
+  display.println("Pressed!");
+  display.display();
+  delay(250);  // Debounce delay
+}
+
+// Temporary function to test button inputs and debouncing
+void displayUpButtonPressed() {
+  display.clearDisplay();
+  display.setTextSize(2);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0, 0);
+  display.println("UP");
+  display.println("Pressed!");
+  display.display();
+  delay(250);  // Debounce delay
+}
 
 
-//Measure and display speed
-void displaySpeed() {
-  float speedMps = gps.f_speed_kmph() / 3.6;
+// Two buttons, one to increase leg length and one to decrease
+// Whenever there's an update to leg length, show the leg length screen, and show the increasing or decreasing leg length
+// After 3 seconds of no input, switch back to efficiency display mode
+// Or just replace the yellow optimal speed with the leg length value
+void updateOptimal() {
+  optimalSpeed = sqrt(legLength * 9.81) / (2 * 3.1415926535)*3.6;
+  optimalPace = 60/optimalSpeed;
+}
+
+
+float calculateEfficiency(float currentSpeed, float optimalSpeed) {
+    float efficiency;
+    // Calculate efficiency as the percentage difference from the optimal speed
+    efficiency = (1 - fabs(currentSpeed - optimalSpeed) / optimalSpeed) * 100.0;
+    // Ensure efficiency doesn't go below 0%
+    if (efficiency < 0) {
+        efficiency = 0;
+    }
+    return efficiency;
+}
+
+// Measure and display speed
+void displayPace() {
+  float currentSpeed = gps.f_speed_kmph();
   char buffer[32]; // Buffer storing the messages to print to the screen
-  
-  // Calculate efficiency
-  float efficiency = (1 - fabs(d_speed - speedMps) / d_speed) * 100.0;
 
+  // Calculate efficiency
+  float efficiency = calculateEfficiency(currentSpeed, optimalSpeed); 
   display.clearDisplay();
   display.setTextSize(2);
   display.setTextColor(SSD1306_WHITE);
   display.setCursor(0, 0);
 
-  sprintf(buffer, "%.2f m/s", d_speed);
-  display.println(buffer);
+  if (currentSpeed > 0) {
+    dtostrf(optimalSpeed, -5, 2, buffer);
+    display.print(buffer);
+    display.println("km/h");
 
-  sprintf(buffer, "%.2f m/s", speedMps);
-  display.println(buffer);
+    dtostrf(currentSpeed, -5, 2, buffer);
+    display.print(buffer);
+    display.println("km/h");
 
-  display.setTextSize(2.5);
-  sprintf(buffer, "%.2f%%", efficiency);
-  display.println(buffer);
-
-  // Extra message if there aren't enough satellites for proper speed data
-  if (speedMps < 0) {
-    display.setTextSize(1.5);
+    display.setTextSize(2.5);
+    dtostrf(efficiency, -5, 2, buffer);
+    display.print(buffer);
+    display.print("%");
+  } else {
+    display.setTextSize(2);
     display.println("Need...");
-    display.println("Satellite...");
+    display.println("Satellites");
     display.println("m(_ _)m");
   }
 
   display.display();
 }
+
