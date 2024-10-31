8a95e23045ad825335f57eb59199bf31327a66ae
Marcus Gee <72676962+mgee-4@users.noreply.github.com>
2024-10-30 22:58:07 -0600

    Updated tuning on harmonic increments
    Now has configurable paces that can be set with buttons

--- walktimizer.ino
+++ walktimizer.ino
@@ -46,7 +46,7 @@ void setup() {
     for (;;);
   }
   display.clearDisplay();
-  updateOptimal();  // Placeholder, need to implement it so that this is called when users adjust leg length manually
+  updateOptimal();
   delay(1000);
 }
 
@@ -56,84 +56,58 @@ void updateState() {
   switch (currentState) {
     case PACE_DISPLAY:
       if (downPressed && upPressed) {
-        currentState = LEG_LENGTH;  // Transition to user input mode
+        currentState = LEG_LENGTH;
       }
-      else if (upPressed && (harmonic <= 5)) {
-        harmonic += 1;
+      else if (upPressed && (harmonic <= 101)) {
+        harmonic += 10;
         updateOptimal();
-        displayUpButtonPressed();  // Display "UP" for up button
       } 
-      else if (downPressed && (harmonic > 0)) {
-        harmonic -= 1;
+      else if (downPressed && (harmonic > 101)) {
+        harmonic -= 10;
         updateOptimal();
-        displayDownButtonPressed();  // Display "DOWN" for down button
       } 
       displayPace();
       break;
 
     case LEG_LENGTH:
-      if (upPressed) {
+      if (upPressed && downPressed) {
+        currentState = PACE_DISPLAY;
+      }
+      else if (upPressed) {
         legLength += 0.1;
         updateOptimal();
-        displayUpButtonPressed();  // Display "UP" for up button
       } 
       else if (downPressed) {
         legLength -= 0.1;
         updateOptimal();
-        displayDownButtonPressed();  // Display "DOWN" for down button
       } 
-      else if (upPressed && downPressed) {
-        currentState = PACE_DISPLAY;  // Return to default state
-      }
       displayLegLength();
       break;
   }
 }
 
-// Temporary function to test button inputs and debouncing
-void displayUpButtonPressed() {
-  display.clearDisplay();
-  display.setTextSize(2);
-  display.setTextColor(SSD1306_WHITE);
-  display.setCursor(0, 0);
-  display.println("UP");
-  display.println("Pressed!");
-  display.display();
-  delay(100);  // Debounce delay
-}
 
-// Temporary function to test button inputs and debouncing
-void displayDownButtonPressed() {
-  display.clearDisplay();
-  display.setTextSize(2);
-  display.setTextColor(SSD1306_WHITE);
-  display.setCursor(0, 0);
-  display.println("DOWN");
-  display.println("Pressed!");
-  display.display();
-  delay(100);  // Debounce delay
-}
-
-// Two buttons, one to increase leg length and one to decrease
-// Whenever there's an update to leg length, show the leg length screen, and show the increasing or decreasing leg length
-// After 3 seconds of no input, switch back to efficiency display mode
-// Or just replace the yellow optimal speed with the leg length value
 void updateOptimal() {
   optimalSpeed = harmonic * sqrt(legLength * 9.81) / ((2 * 3.1415926535)*3.6);
   optimalPace = speedToPace(optimalSpeed);
 }
 
-float calculateEfficiency(float currentPace, float optimalPace) {
+
+float calculateEfficiency(float currentSpeed, float optimalSpeed) {
     float efficiency;
-    // Calculate efficiency as the percentage difference from the optimal speed
-    efficiency = (1 - (fabs(optimalPace - currentPace) / optimalPace)) * 100.0;
+    efficiency = (1 - (fabs(optimalSpeed - currentSpeed) / optimalSpeed)) * 100.0;
+    if (efficiency < 0){
+      efficiency = 0;
+    }
     return efficiency;
 }
+
 
 float speedToPace(float currentSpeed) {
   float currentPace = 60 / currentSpeed;
   return currentPace;
 }
+
 
 void formatPace(float pace, char* buffer) {
     int minutes = (int)pace;
@@ -141,12 +115,15 @@ void formatPace(float pace, char* buffer) {
     snprintf(buffer, 32, "%d'%02d\"", minutes, seconds);
 }
 
+
 void displayPace() {
+  float currentSpeed = gps.f_speed_kmph();
   float currentPace = speedToPace(gps.f_speed_kmph());
   char buffer[32];
 
   // Calculate efficiency
-  float efficiency = calculateEfficiency(currentPace, optimalPace); 
+  updateOptimal();
+  float efficiency = calculateEfficiency(currentSpeed, optimalSpeed); 
   display.clearDisplay();
   display.setTextSize(2);
   display.setTextColor(SSD1306_WHITE);
@@ -194,7 +171,6 @@ void displayLegLength() {
   display.display();
 }
 
-// Need to set up FSM between user input mode and speed checking/displaying, currently just printing speed/efficiency
 void loop() {
   while (Serial1.available()) {
     char c = Serial1.read();
